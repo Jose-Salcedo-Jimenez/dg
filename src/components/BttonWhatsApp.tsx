c373029ddd8a05c23d59fb6f6365bb4e41bd9007
Jose Salcedo Jimenez <josesslcedo@gmail.com>
2026-02-23 08:58:57 -0500

    Modified WhatsApp icon

--- src/components/BttonWhatsApp.tsx
+++ src/components/BttonWhatsApp.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image"
 export default function BttonWhatsApp() {
   return (
-    <div className="bg-green-400 w-12 h-12 rounded-full fixed right-5 bottom-40 flex justify-center items-center z-50">
+    <div className="w-12 h-12 rounded-full fixed right-5 bottom-40 flex justify-center items-center z-50">
         <a href="https://wa.link/s1josl">
             <Image
             src={"/Icons/whatsapp.png"}
-            width={30}
-            height={30}
+            width={52}
+            height={52}
             alt="Icono de WhatsApp"
             className="pl-[0.2rem]"
             />
